eb29bd787f367d3ba618efc5a2e4c13c7026772a
racionalVol1 <guilhermeperondi09@gmail.com>
2025-02-11 13:25:41 -0300

    Validação de idade

---
+++ assets/js/validacao.js
@@ -0,0 +1,36 @@
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo   
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input)
+    }
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
+
+const dataNascimento = document.querySelector('#date');
+
+dataNascimento.addEventListener('blur', (evento) => {
+    validaDataNascimento(evento.target);
+});
+
+function validaDataNascimento(input){
+    const dataRecebida = new Date(input.value);
+    let mensagem = "";
+
+    if(!maiorQue18(dataRecebida)) {
+    mensagem = 'Você deve ser maior de 18 anos para prosseguir';
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+   return dataMais18 <= dataAtual;
+
+
+}
