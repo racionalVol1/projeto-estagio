7704dd11455833576d870015f3ed90bbfcfd3731
racionalVol1 <guilhermeperondi09@gmail.com>
2025-02-11 14:21:14 -0300

    Validação

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -3,7 +3,42 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
     }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.mensagem-erro').innerHTML = monstraMenagemDeErro(tipoDeInput, input)
+     }
+
 }
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio'
+    },
+    email: {
+        valueMissing: 'O campo email não pode estar vazio',
+        typeMismatch: 'O email digitado não é válido'
+    },
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, pelo menos um número, uma letra maiúscula e uma minúscula'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo data de nascimento não pode estar vazio',
+        customError: 'Você deve ser maior que 18 anos para prosseguir'
+    }
+}
+
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
@@ -14,6 +49,17 @@ const dataNascimento = document.querySelector('#date');
 dataNascimento.addEventListener('blur', (evento) => {
     validaDataNascimento(evento.target);
 });
+
+function mostraMensagemDeErro(tipoDeInput, input){
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]){
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+
+    return mensagem
+}
 
 function validaDataNascimento(input){
     const dataRecebida = new Date(input.value);
