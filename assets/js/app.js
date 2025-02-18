6ddfceacb1eff5485fc67a2c54a7c2861c82d0d4
Guilherme Perondi <guilhermeperondi09@gmail.com>
2025-02-18 13:58:45 -0300

    Armazenando dados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,59 @@
-console.log('Hello from app.js');
+function cadastraUsario (){
+    var nome = document.getElementById('nome');
+    var email = document.getElementById('email');
+    var dataNascimento = document.getElementById('dataNascimento');
+    var celular = document.getElementById('celular');
+    var senha = document.getElementById('senha');
+    var confirmaSenha = document.getElementById('confirmaSenha');
+
+    var dados = JSON.parse(localStorage.getItem('dados'));
+
+    if (dados == null){
+        localStorage.setItem('dados', '[]');
+        dados = []; 
+    }  
+
+    var registro = {
+        nome: nome.value,
+        email: email.value,
+        dataNascimento: dataNascimento.value,
+        celular: celular.value,
+        senha: senha.value,
+        confirmaSenha: confirmaSenha.value
+    }
+
+    dados.push(registro);
+
+    localStorage.setItem('dados', JSON.stringify(dados));
+    alert('Cadastro realizado com sucesso!');
+}
+
+function cadastraArtista (){
+    var nome = document.getElementById('nome');
+    var email = document.getElementById('email');
+    var estiloMusical = document.getElementById('estiloMusical');
+    var celular = document.getElementById('celular');
+    var senha = document.getElementById('senha');
+    var confirmaSenha = document.getElementById('confirmaSenha');
+
+    var dados = JSON.parse(localStorage.getItem('dados'));
+
+    if (dados == null){
+        localStorage.setItem('dados', '[]');
+        dados = []; 
+    }  
+
+    var registro = {
+        nome: nome.value,
+        email: email.value,
+        dataNascimento: dataNascimento.value,
+        celular: celular.value,
+        senha: senha.value,
+        confirmaSenha: confirmaSenha.value
+    }
+
+    dados.push(registro);
+
+    localStorage.setItem('dados', JSON.stringify(dados));
+    alert('Cadastro realizado com sucesso!');
+}
