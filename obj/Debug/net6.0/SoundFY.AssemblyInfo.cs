bcbdedc9985005036900a6644616c242fab43190
racionalVol1 <guilhermeperondi09@gmail.com>
2025-02-26 10:06:36 -0300

    a

--- obj/Debug/net6.0/SoundFY.AssemblyInfo.cs
+++ obj/Debug/net6.0/SoundFY.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundFY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea7a5f4777e122d95af838c77c9aaf7dae13195")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundFY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundFY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
